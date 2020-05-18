3e5bd8e79d60b38c09550b8b0aae90e31a3c52ef
Olusola Olagoroye <solaolagoroye@gmail.com>
2020-05-18 09:36:01 -0500

    set up store as a prop in provider

--- teas-frontend/src/index.js
+++ teas-frontend/src/index.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import {provider} from 'redux-thunk'
 import './index.css';
 import App from './App';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancers(
+const store = createStore(teaReducer, composeEnhancers(
     applyMiddleware(thunk)
   ));
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
