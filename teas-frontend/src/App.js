684145e7a3b9c5c5f9cc8eaa379b4957cf8d9c41
Olusola Olagoroye <solaolagoroye@gmail.com>
2020-05-18 04:31:37 -0500

    installed and imported packages to index.js

---
+++ teas-frontend/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+// import './App.css';
+// class componet
+class App extends React.Component {
+//   componentDidMount(){
+    
+//     fetch('http://localhost:3000/api/v1/teas/1/reviews', {
+//       method: 'GET'
+//     })
+    
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+// }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          
+          App
+        </header>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
